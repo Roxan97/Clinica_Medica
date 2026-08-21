53c0973ad97af4fea4ba58aeb0fe54e9bddd05f6
Roxan97 <soyingrid.1977@gmail.com>
2026-08-21 17:49:40 -0600

    Semana 5, componentes avanzados

--- Clinica_Medica/frmPrincipal.Designer.cs
+++ Clinica_Medica/frmPrincipal.Designer.cs
@@ -35,11 +35,20 @@
             this.btnGuardar = new System.Windows.Forms.Button();
             this.txtEdadPaciente = new System.Windows.Forms.TextBox();
             this.lbGeneroPaciente = new System.Windows.Forms.Label();
-            this.rbMasculino = new System.Windows.Forms.RadioButton();
-            this.rbFemenino = new System.Windows.Forms.RadioButton();
             this.txtApellidoPaciente = new System.Windows.Forms.TextBox();
             this.txtNombrePaciente = new System.Windows.Forms.TextBox();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.cmbEspecialidad = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPacientes = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataPacientes = new System.Windows.Forms.DataGridView();
             this.panelInformacion.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPacientes.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPacientes)).BeginInit();
             this.SuspendLayout();
             // 
             // lbNombrePaciente
@@ -74,11 +83,14 @@
             // 
             // panelInformacion
             // 
+            this.panelInformacion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelInformacion.Controls.Add(this.label1);
+            this.panelInformacion.Controls.Add(this.cmbEspecialidad);
+            this.panelInformacion.Controls.Add(this.cmbGenero);
             this.panelInformacion.Controls.Add(this.btnGuardar);
             this.panelInformacion.Controls.Add(this.txtEdadPaciente);
             this.panelInformacion.Controls.Add(this.lbGeneroPaciente);
-            this.panelInformacion.Controls.Add(this.rbMasculino);
-            this.panelInformacion.Controls.Add(this.rbFemenino);
             this.panelInformacion.Controls.Add(this.txtApellidoPaciente);
             this.panelInformacion.Controls.Add(this.txtNombrePaciente);
             this.panelInformacion.Controls.Add(this.lbNombrePaciente);
@@ -86,14 +98,14 @@
             this.panelInformacion.Controls.Add(this.lbApellidoPaciente);
             this.panelInformacion.Location = new System.Drawing.Point(12, 12);
             this.panelInformacion.Name = "panelInformacion";
-            this.panelInformacion.Size = new System.Drawing.Size(590, 277);
+            this.panelInformacion.Size = new System.Drawing.Size(628, 284);
             this.panelInformacion.TabIndex = 3;
             // 
             // btnGuardar
             // 
             this.btnGuardar.BackColor = System.Drawing.Color.LawnGreen;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(276, 178);
+            this.btnGuardar.Location = new System.Drawing.Point(390, 217);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(127, 42);
             this.btnGuardar.TabIndex = 9;
@@ -120,30 +132,6 @@
             this.lbGeneroPaciente.TabIndex = 7;
             this.lbGeneroPaciente.Text = "Genero Paciente";
             // 
-            // rbMasculino
-            // 
-            this.rbMasculino.AutoSize = true;
-            this.rbMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbMasculino.Location = new System.Drawing.Point(321, 131);
-            this.rbMasculino.Name = "rbMasculino";
-            this.rbMasculino.Size = new System.Drawing.Size(43, 22);
-            this.rbMasculino.TabIndex = 6;
-            this.rbMasculino.TabStop = true;
-            this.rbMasculino.Text = "M";
-            this.rbMasculino.UseVisualStyleBackColor = true;
-            // 
-            // rbFemenino
-            // 
-            this.rbFemenino.AutoSize = true;
-            this.rbFemenino.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbFemenino.Location = new System.Drawing.Point(202, 131);
-            this.rbFemenino.Name = "rbFemenino";
-            this.rbFemenino.Size = new System.Drawing.Size(39, 22);
-            this.rbFemenino.TabIndex = 5;
-            this.rbFemenino.TabStop = true;
-            this.rbFemenino.Text = "F";
-            this.rbFemenino.UseVisualStyleBackColor = true;
-            // 
             // txtApellidoPaciente
             // 
             this.txtApellidoPaciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -162,19 +150,96 @@
             this.txtNombrePaciente.TabIndex = 3;
             this.txtNombrePaciente.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombrePaciente_KeyPress);
             // 
+            // cmbGenero
+            // 
+            this.cmbGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Location = new System.Drawing.Point(202, 132);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(201, 24);
+            this.cmbGenero.TabIndex = 10;
+            // 
+            // cmbEspecialidad
+            // 
+            this.cmbEspecialidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEspecialidad.FormattingEnabled = true;
+            this.cmbEspecialidad.Location = new System.Drawing.Point(202, 170);
+            this.cmbEspecialidad.Name = "cmbEspecialidad";
+            this.cmbEspecialidad.Size = new System.Drawing.Size(201, 24);
+            this.cmbEspecialidad.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Genero Paciente";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabPacientes);
+            this.panel1.Location = new System.Drawing.Point(13, 303);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(627, 260);
+            this.panel1.TabIndex = 4;
+            // 
+            // tabPacientes
+            // 
+            this.tabPacientes.Controls.Add(this.tabPage1);
+            this.tabPacientes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabPacientes.Location = new System.Drawing.Point(0, 0);
+            this.tabPacientes.Name = "tabPacientes";
+            this.tabPacientes.SelectedIndex = 0;
+            this.tabPacientes.Size = new System.Drawing.Size(627, 260);
+            this.tabPacientes.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataPacientes);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(619, 231);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataPacientes
+            // 
+            this.dataPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPacientes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataPacientes.Location = new System.Drawing.Point(3, 3);
+            this.dataPacientes.Name = "dataPacientes";
+            this.dataPacientes.RowHeadersWidth = 51;
+            this.dataPacientes.RowTemplate.Height = 24;
+            this.dataPacientes.Size = new System.Drawing.Size(613, 225);
+            this.dataPacientes.TabIndex = 0;
+            this.dataPacientes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataPacientes_CellDoubleClick);
+            this.dataPacientes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataPacientes_KeyDown);
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(614, 329);
+            this.ClientSize = new System.Drawing.Size(652, 575);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelInformacion);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Informacion";
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmPrincipal_KeyDown);
             this.panelInformacion.ResumeLayout(false);
             this.panelInformacion.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tabPacientes.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataPacientes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -185,13 +250,18 @@
         private System.Windows.Forms.Label lbApellidoPaciente;
         private System.Windows.Forms.Label lbEdadPaciente;
         private System.Windows.Forms.Panel panelInformacion;
-        private System.Windows.Forms.RadioButton rbFemenino;
         private System.Windows.Forms.TextBox txtApellidoPaciente;
         private System.Windows.Forms.TextBox txtNombrePaciente;
-        private System.Windows.Forms.RadioButton rbMasculino;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.TextBox txtEdadPaciente;
         private System.Windows.Forms.Label lbGeneroPaciente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbEspecialidad;
+        private System.Windows.Forms.ComboBox cmbGenero;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabPacientes;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataPacientes;
     }
 }
 
--- Clinica_Medica/frmPrincipal.cs
+++ Clinica_Medica/frmPrincipal.cs
@@ -12,9 +12,43 @@ namespace Clinica_Medica
 {
     public partial class frmPrincipal : Form
     {
+        DataTable pacientes = new DataTable();
         public frmPrincipal()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += frmPrincipal_KeyDown;
+
+            cmbGenero.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbEspecialidad.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            //Llenando los list de los cmbox
+
+            cmbGenero.Items.Add("Masculino");
+            cmbGenero.Items.Add("Femenino");
+            cmbGenero.Items.Add("Otro");
+
+            cmbEspecialidad.Items.Add("Medicina General");
+            cmbEspecialidad.Items.Add("Pediatria");
+            cmbEspecialidad.Items.Add("Cardiologia");
+
+            //Crear las columnas de la tabla en memoria
+
+            pacientes.Columns.Add("Nombre",typeof(string));
+            pacientes.Columns.Add("Apellido", typeof(string));
+            pacientes.Columns.Add("Edad", typeof(int));
+            pacientes.Columns.Add("Genero", typeof(string));
+            pacientes.Columns.Add("Especialidad", typeof(string));
+
+            //Enlazar los datos con el datagridview
+
+            dataPacientes.DataSource = pacientes;
+
+            dataPacientes.AllowUserToAddRows = false;
+            dataPacientes.ReadOnly = true;
+            dataPacientes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataPacientes.MultiSelect = false;
+
         }
 
         private void txtEdadPaciente_KeyPress(object sender, KeyPressEventArgs e)
@@ -37,16 +71,76 @@ namespace Clinica_Medica
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            string nombre =txtNombrePaciente.Text;
+            /*string nombre =txtNombrePaciente.Text;
             string apellido = txtApellidoPaciente.Text;
             string edad = txtEdadPaciente.Text;
-            MessageBox.Show($"El nombre ingresado es : {nombre} {apellido} \n Edad: {edad} años", "Informacion Paciente");
+            MessageBox.Show($"El nombre ingresado es : {nombre} {apellido} \n Edad: {edad} años", "Informacion Paciente");*/
+            try
+            {
+                // Validar que todos los campos estén llenos
+                if (string.IsNullOrWhiteSpace(txtNombrePaciente.Text) ||
+                    string.IsNullOrWhiteSpace(txtApellidoPaciente.Text) ||
+                    string.IsNullOrWhiteSpace(txtEdadPaciente.Text) ||
+                    cmbGenero.SelectedIndex == -1 ||
+                    cmbEspecialidad.SelectedIndex == -1)
+                {
+                    MessageBox.Show(
+                        "Por favor, complete todos los campos.",
+                        "Campos incompletos",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                    );
+
+                    return;
+                }
+
+                int edad = int.Parse(txtEdadPaciente.Text);
+
+                //Agregar el paciente a la tabla a nivelde memoria
+
+                pacientes.Rows.Add(
+                    txtNombrePaciente.Text.Trim().ToUpper(),
+                    txtApellidoPaciente.Text.Trim().ToUpper(),
+                    edad,
+                    cmbGenero.Text.ToUpper(),
+                    cmbEspecialidad.Text.ToUpper());
+
+                MessageBox.Show(
+                    "Paciente guardado correctamente.",
+                    "Información",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                //Limpiar controles
+
+                txtNombrePaciente.Clear();
+                txtApellidoPaciente.Clear();
+                txtEdadPaciente.Clear();
+                cmbGenero.SelectedIndex = -1;
+                cmbEspecialidad.SelectedIndex = -1;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Ocurrió un error al guardar el paciente: " + ex.Message,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        
+        
+        
+        
         }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            txtNombrePaciente.Text = "Roxana";
-            txtApellidoPaciente.Text = "Argueta";
+            txtNombrePaciente.Text = " ";
+            txtApellidoPaciente.Text = " ";
 
         }
 
@@ -58,6 +152,41 @@ namespace Clinica_Medica
                 MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
 
+        }
+
+        private void dataPacientes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            DataGridViewRow fila = dataPacientes.Rows[e.RowIndex];
+
+            DialogResult resultado = MessageBox.Show(
+                "Desea eliminar este paciente",
+                "Confirmar Eliminacion",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+                );
+
+            if (resultado == DialogResult.Yes)
+            {
+                //Eliminar de la tabla en memoria la fila seleccionada
+                pacientes.Rows.RemoveAt(e.RowIndex);
+            }
+        }
+
+        private void dataPacientes_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void frmPrincipal_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                SelectNextControl(this.ActiveControl, true, true, true, true);
+            }
         }
     }
 }
